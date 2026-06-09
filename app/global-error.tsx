b574264a4e9f2c1574b7be862952d55f5e6cec7b
Zhaoan Liu <zhaoanl@yahoo.com>
2026-06-09 11:18:52 -0700

    test: revert global-error.tsx to broken state to verify regression detection

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import * as Sentry from '@sentry/nextjs'
 import { useEffect } from 'react'
 
 export default function GlobalError({
@@ -10,7 +11,7 @@ export default function GlobalError({
   reset: () => void
 }) {
   useEffect(() => {
-    console.error(error)
+    Sentry.captureException(error)
   }, [error])
 
   return (
